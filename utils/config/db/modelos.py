161814dffda1894d08b623c1506b64721482f0f6
Yado <imneokuze@gmail.com>
2020-12-05 11:01:29 -0800

    Create modelos.py

---
+++ utils/config/db/modelos.py
@@ -0,0 +1,24 @@
+DiscordGuild = """
+    CREATE TABLE IF NOT EXISTS DiscordGuild (
+        id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+        name TEXT NOT NULL UNIQUE,
+        lang TEXT,
+        suffix TEXT,
+        created_at TEXT,
+        venable INTEGER DEFAULT 0,
+        vrole TEXT,
+        vchannel TEXT
+    )
+"""
+
+DiscordUser = """
+    CREATE TABLE IF NOT EXISTS DiscordUser (
+        id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+        name TEXT NOT NULL UNIQUE,
+        lang TEXT,
+        suffix TEXT,
+        nick TEXT,
+        birthday TEXT,
+        created_at TEXT,
+    )
+"""
